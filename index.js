5d94a441d63751a72ff3564bb842ac7fb437d4bb
naina608 <104517150+naina608@users.noreply.github.com>
2023-02-03 18:29:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+setInterval(()=>{
+    d=new Date();
+    htime=d.getHours();
+    mtime=d.getMinutes();
+    stime=d.getSeconds();
+    hroation=30*htime+mtime/2;
+    mrotation=6*mtime;
+    srotation=6*stime;
+    hour.style.transform=`rotate(${hroation}deg)`;
+    minute.style.transform=`rotate(${mrotation}deg)`;
+    second.style.transform=`rotate(${srotation}deg)`;
+})
